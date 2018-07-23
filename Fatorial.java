4383c71fa7ad8ab2b63ec9900451907b55fb43e5
Dirceu Coques Junior <dirceu@brq.com>
2018-07-23 16:05:00 -0300

    inclusao de teste para numero negativo

--- Fatorial.java
+++ Fatorial.java
@@ -1,8 +1,11 @@
 public class Fatorial {
 	
 	// versao iterativa do calculo do fatorial
-	public int calcularFatorial(int n) {
+	public int calcularFatorial(int n) throws IllegalArgumentException {
 		int resultado = 1;
+		
+		if (n < 0)
+			throw new IllegalArgumentException("Excecao gerada");
 		
 		for (int i = 2; i <= n; i++)
 			resultado *= i;
