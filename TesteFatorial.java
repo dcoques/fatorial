b6e8c63b598e8270f562001cb3ae4bb3514ea74e
Dirceu Coques Junior <dirceu@brq.com>
2018-07-26 15:57:10 -0300

    exclusao de um item de calculo de fatorial

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -4,7 +4,6 @@ public class TesteFatorial {
 		Fatorial f = new Fatorial();
 		
 		//Exibe o fatorial
-		System.out.println(f.calcularFatorial(0));
 		System.out.println(f.calcularFatorial(1));
 		System.out.println(f.calcularFatorial(5));
 		System.out.println(f.calcularFatorial(10));
